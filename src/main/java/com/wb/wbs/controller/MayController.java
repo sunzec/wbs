1b3342dc86e3d8dddfbc8ce1f32b68386c4a36e9
sunzec <szw635156582@163.com>
2020-12-31 14:33:15 +0800

    修改数据库配置

--- src/main/java/com/wb/wbs/controller/MayController.java
+++ src/main/java/com/wb/wbs/controller/MayController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
